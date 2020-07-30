90c4cef9e832ed0a374b190ed88828cb9cfbabfa
egoldshm <39948614+egoldshm@users.noreply.github.com>
2020-07-30 11:57:15 +0300

    change2

--- manage.py
+++ manage.py
@@ -2,31 +2,25 @@
 import os
 import sys
 
-from telegram import Bot
-from telegram.ext import Updater, Dispatcher
+import telebot
+
+TOKEN = "1085962867:AAHQyGzmCyKJDfXGNmBgGVpt6Knb_eSzdE8"
+
+bot = telebot.TeleBot(TOKEN)
 
 
 def start_callback(update, context):
     update.message.reply_text("Welcome to my awesome bot!")
 
 
-def main():
-    import os
-
-    TOKEN = "1085962867:AAHQyGzmCyKJDfXGNmBgGVpt6Knb_eSzdE8"
-    PORT = int(os.environ.get('PORT', '8443'))
-    updater = Updater(TOKEN)
-    updater.start_webhook(listen="0.0.0.0",
-                          port=PORT,
-                          url_path=TOKEN)
-    updater.bot.set_webhook("https:///mysterious-sierra-44668.herokuapp.com/" + TOKEN)
-
-    updater.dispatcher.add_handler(start_callback)
-
-    updater.idle()
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 
-print("started 1", file=open('file.txt', 'w'))
-main()
-print("ended 1", file=open('file.txt', 'w'))
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
+
+bot.polling()
